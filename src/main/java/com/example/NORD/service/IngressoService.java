e7748e75c2a02b5171759c3c110717b39e839771
CrashBrazil <vmatheusvenanciodasilva@gmail.com>
2025-09-25 13:29:29 -0300

    RepositorioIngresso,IngressoDto add

--- src/main/java/com/example/NORD/service/IngressoService.java
+++ src/main/java/com/example/NORD/service/IngressoService.java
@@ -1,13 +1,27 @@
 package com.example.NORD.service;
 
 
+import com.example.NORD.Repositorio.RepositorioIngresso;
+import com.example.NORD.mapstruct.MapStruct;
+import com.example.NORD.model.DTO.IngressoDto;
+import com.example.NORD.model.Ingresso;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+
 public class IngressoService {
+
+    private final RepositorioIngresso repositorioIngresso;
+
+    public void criarIngresso(IngressoDto ingressoDto){
+        Ingresso ingresso = MapStruct.INSTANCE.converter_ingresso(ingressoDto);
+
+        repositorioIngresso.save(ingresso);
+    }
     public void associarIngresso(){
+
 
 
     }
